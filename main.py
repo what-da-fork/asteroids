6db64eeb146e79e5c4b28342e290509dddd46dca
what-da-fork <mason@wyd.mozmail.com>
2025-09-04 18:26:37 -0700

    updated framerate to 60 and stored the delta in dt variable

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from constants import *
 def main():
     pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, "black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
