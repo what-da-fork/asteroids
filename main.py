3c83dbb528f753e6d362e5202f6d39954ec7dd7f
what-da-fork <mason@wyd.mozmail.com>
2025-09-07 14:23:20 -0700

    added the player

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     pygame.init
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
@@ -11,6 +15,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, "black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
